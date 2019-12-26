1b12ea30e25918fcf97abde3735fe80e53cc01ec
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2019-12-26 16:33:03 +0700

    added post by id handler

--- src/posts/components/CommentItem.js
+++ src/posts/components/CommentItem.js
@@ -27,25 +27,30 @@ const comments = [
 
 const CommentItem = props => {
   const loadedComments = comments.filter(comment => comment.postId === props.postId)
-  return (
-    <React.Fragment>
-      <div className="w-full p-3">
-        <div className="bg-white border rounded shadow p-5">
-          {loadedComments.map(comment => (
-            <React.Fragment key={comment.commentId}>
-              <p className="text-gray-700 text-base">
-                {comment.body}
-              </p>
-              <div className="text-grey-dark leading-normal text-sm py-2">
-                  <p>{comment.author.name} <span className="mx-1 text-xs">•</span> { comment.created_at }</p>
-              </div>
-            </React.Fragment>
-          ))}
+
+  if (loadedComments.length) {
+    return (
+      <div classNames="flex flex-row flex-wrap flex-grow mt-2">
+        <div className="w-full p-3">
+          <div className="bg-white border rounded shadow p-5">
+            {loadedComments.map(comment => (
+              <React.Fragment key={comment.commentId}>
+                <p className="text-gray-700 text-base">
+                  {comment.body}
+                </p>
+                <div className="text-grey-dark leading-normal text-sm py-2">
+                    <p>{comment.author.name} <span className="mx-1 text-xs">•</span> { comment.created_at }</p>
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
+          <hr className="border-b-1 border-gray-100" />
         </div>
-        <hr className="border-b-1 border-gray-100" />
       </div>
-    </React.Fragment>
-  )
+    )
+  } else {
+    return null;
+  }
 }
 
 export default CommentItem;
--- src/posts/pages/Post.js
+++ src/posts/pages/Post.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import PostItem from '../components/PostItem';
 import MainNavigation from '../../shared/components/Navigation/MainNavigation';
@@ -7,83 +7,92 @@ import CommentItem from '../components/CommentItem';
 import { useParams } from 'react-router-dom';
 
 import { AuthContext } from '../../shared/context/auth-context';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 
-const POSTS = [{
-    id: 'post1',
-    title: 'Title Example',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt pretium fringilla. Etiam vitae est et tortor tristique cursus. Nam consequat velit eget ante tempor tincidunt. Donec velit nisi, posuere lacinia feugiat non, porta sit amet sem. Etiam euismod imperdiet maximus. Quisque eu diam ut massa mollis rhoncus. Pellentesque sit amet velit at elit rhoncus consequat ut eu diam. Ut eleifend ligula nisi, sit amet pellentesque odio vestibulum at. Nullam bibendum diam et velit auctor accumsan.',
-    imageUrl: 'http://localhost:3000/sample-post.jpg',
-    user: 'Walter White',
-    creator: 'user1',
-    date: new Date().toLocaleDateString(),
-    responses: [{
-      reaction: 'happy',
-      comment: 'this is cool'
-    }]
-  },
-  {
-    id: 'test2',
-    title: 'Title Example',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt pretium fringilla. Etiam vitae est et tortor tristique cursus. Nam consequat velit eget ante tempor tincidunt. Donec velit nisi, posuere lacinia feugiat non, porta sit amet sem. Etiam euismod imperdiet maximus. Quisque eu diam ut massa mollis rhoncus. Pellentesque sit amet velit at elit rhoncus consequat ut eu diam. Ut eleifend ligula nisi, sit amet pellentesque odio vestibulum at. Nullam bibendum diam et velit auctor accumsan.', //TODO: parse dulu
-    imageUrl: 'http://localhost:3000/sample-post.jpg',
-    user: 'Walter White',
-    creator: 'user1',
-    date: new Date().toLocaleDateString(),
-    commentsId: ['comment1']
-  }
-];
 
 const Post = () => {
   const auth = useContext(AuthContext);
   const postId = useParams().postId;
-  const loadedItem = POSTS.filter(post => post.id === postId)[0];
+  const userId = useParams().userId;
+
+  const [loadedItem, setLoadedItem] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/posts/${postId}`
+        );
+        setLoadedItem(responseData.post);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+  // const loadedItem = POSTS.filter(post => post.id === postId)[0];
 
   if(loadedItem) {
     return (
-      <div>
+      <React.Fragment>
         <MainNavigation/>
-        <div className="container w-full mx-auto pt-2">
-          <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
-            <div className="flex flex-row flex-wrap flex-grow mt-2">
-              <PostItem post={loadedItem} show={true}/>
-            </div>
-            <div className="flex flex-row flex-wrap flex-grow mt-2">
-              <CommentItem postId={postId}/>˝
-            </div>
-            {auth.isLoggedIn && (
-              <div className="flex flex-row flex-wrap flex-grow mt-2">
-                <CommentsManager/>
-              </div>
-            )}
-            {!auth.isLoggedIn && (
-              <div className="flex flex-row flex-wrap flex-grow mt-2">
-                <div className="w-full p-3">
-                  <div className="bg-white border rounded shadow p-5">
-                    <p className="text-gray-700 text-base">
-                      Login to add a comment.
-                    </p>
-                  </div>
-                  <hr className="border-b-1 border-gray-100" />
-                </div>
-              </div>
-            )}
+        <ErrorModal error={error} onClear={clearError} />
+        {isLoading && (
+          <div className="text-center flex justify-center content-center">
+            <LoadingSpinner />
           </div>
+        )}
+        {!isLoading && loadedItem &&
+          <div className="container w-full mx-auto pt-2">
+            <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+              <div className="flex flex-row flex-wrap flex-grow mt-2">
+                <PostItem post={loadedItem} show={true}/>
+              </div>
+              <CommentItem postId={postId}/>
+              {auth.isLoggedIn && (
+                <div className="flex flex-row flex-wrap flex-grow mt-2">
+                  <CommentsManager/>
+                </div>
+              )}
+              {!auth.isLoggedIn && (
+                <div className="flex flex-row flex-wrap flex-grow mt-2">
+                  <div className="w-full p-3">
+                    <div className="bg-white border rounded shadow p-5">
+                      <p className="text-gray-700 text-base">
+                        Login to add a comment.
+                      </p>
+                    </div>
+                    <hr className="border-b-1 border-gray-100" />
+                  </div>
+                </div>
+              )}
+            </div>
         </div>
-      </div>
+        }
+      </React.Fragment>
     );
   } else {
     return (
-      <div>
+      <React.Fragment>
         <MainNavigation/>
-        <div className="container w-full mx-auto pt-2">
-          <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
-            <div className="flex flex-row flex-wrap flex-grow mt-2">
-              <PostItem show={false}/>
+        <ErrorModal error={error} onClear={clearError} />
+        {isLoading && (
+          <div className="text-center flex justify-center content-center">
+            <LoadingSpinner />
+          </div>
+        )}
+        {!isLoading && loadedItem &&
+          <div className="container w-full mx-auto pt-2">
+            <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+              <div className="flex flex-row flex-wrap flex-grow mt-2">
+                <PostItem show={false}/>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        }
+      </React.Fragment>
     );
   }
 }
