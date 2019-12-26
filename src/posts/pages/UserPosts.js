a4deca1f1cd78636f99307647517f70522556c93
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2019-12-26 15:22:50 +0700

    added get post by id handler

--- src/posts/pages/UserPosts.js
+++ src/posts/pages/UserPosts.js
@@ -1,48 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Contents from "../components/Contents";
 import Table from "../components/Table";
 import MainNavigation from '../../shared/components/Navigation/MainNavigation';
-
-const DUMMY_POSTS = [{
-    id: 'post1',
-    title: 'Title Example',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt pretium fringilla. Etiam vitae est et tortor tristique cursus. Nam consequat velit eget ante tempor tincidunt. Donec velit nisi, posuere lacinia feugiat non, porta sit amet sem. Etiam euismod imperdiet maximus. Quisque eu diam ut massa mollis rhoncus. Pellentesque sit amet velit at elit rhoncus consequat ut eu diam. Ut eleifend ligula nisi, sit amet pellentesque odio vestibulum at. Nullam bibendum diam et velit auctor accumsan.',
-    imageUrl: 'http://localhost:3000/sample-post.jpg',
-    user: 'Walter White',
-    creator: 'user1',
-    date: new Date().toLocaleDateString(),
-    commentsId: ['comment1']
-  },
-  {
-    id: 'test2',
-    title: 'Title Example',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt pretium fringilla. Etiam vitae est et tortor tristique cursus. Nam consequat velit eget ante tempor tincidunt. Donec velit nisi, posuere lacinia feugiat non, porta sit amet sem. Etiam euismod imperdiet maximus. Quisque eu diam ut massa mollis rhoncus. Pellentesque sit amet velit at elit rhoncus consequat ut eu diam. Ut eleifend ligula nisi, sit amet pellentesque odio vestibulum at. Nullam bibendum diam et velit auctor accumsan.',
-    imageUrl: 'http://localhost:3000/sample-post.jpg',
-    user: 'Walter White',
-    creator: 'user2',
-    date: new Date().toLocaleDateString(),
-    commentsId: ['comment2']
-  }
-];
-// TODO: ini jadi patokan api nanti
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const UserPosts = () => {
+  const [loadedPosts, setLoadedPosts] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const userId = useParams().userId;
-  const loadedPosts = DUMMY_POSTS.filter(place => place.creator === userId);
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/posts/user/${userId}`
+        );
+        setLoadedPosts(responseData.posts);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
 
   return (
-    <div>
-      <MainNavigation/>
-      <div className="container w-full mx-auto pt-2">
-        <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
-          <Contents items={loadedPosts}/>
-          <hr className="border-b-2 border-gray-400 my-8 mx-4" />
-          <Table/>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="text-center flex justify-center content-center">
+          <LoadingSpinner />
         </div>
-      </div>
-    </div>
+      )}
+      <MainNavigation/>
+      {!isLoading && loadedPosts &&
+        <div className="container w-full mx-auto pt-2">
+          <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+            <Contents items={loadedPosts}/>
+            <hr className="border-b-2 border-gray-400 my-8 mx-4" />
+            <Table/>
+          </div>
+        </div>
+      }
+    </React.Fragment>
   );
 }
 
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -32,7 +32,7 @@ const NavLinks = () => {
       <div className="hidden sm:flex sm:items-center">
         {auth.isLoggedIn && (
           <Link
-            to="/user1/posts"
+            to={`/${auth.userId}/posts`}
             className="border text-sm font-semibold border px-4 py-2 rounded-lg mr-4"
           >
             My Posts
